a6c9b864f37d173309c5f81dcfb999aa525cfe82
Vishal <anuj@inwizards.com>
2024-09-25 15:25:33 +0530

    update content

--- amazon_connector_inwizards/__manifest__.py
+++ amazon_connector_inwizards/__manifest__.py
@@ -76,6 +76,7 @@
             '/amazon_connector_inwizards/static/src/components/custom.css',
             'amazon_connector_inwizards/static/src/components/amazon_dashboard.js',
             'amazon_connector_inwizards/static/src/components/amazon_dashboard.xml',
+            '/amazon_connector_inwizards/static/src/components/content_page.css',
         ],
     }
     
